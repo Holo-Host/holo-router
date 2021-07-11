c4e18c9c6f2f528472eca1e523e0a121febb182f
zo-el <joelulahanna@gmail.com>
2021-07-11 13:09:59 +0530

    update hpos-config in agent

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -15,7 +15,9 @@ fn serialize_holochain_agent_id<S>(public_key: &PublicKey, serializer: S) -> Res
 where
     S: Serializer,
 {
-    serializer.serialize_str(&public_key::to_base36_id(&public_key))
+    serializer.serialize_str(&public_key::holochain_pub_key_encoding(
+        &public_key.to_bytes(),
+    ))
 }
 
 fn serialize_instant<S>(time: &SystemTime, serializer: S) -> Result<S::Ok, S::Error>
@@ -38,52 +40,53 @@ struct Payload {
     zerotier_address: zerotier::Address,
 }
 
+#[derive(Debug, Fail)]
+pub enum RouterError {
+    #[fail(display = "Invalid config version used. please upgrade to hpos-config v2")]
+    ConfigVersionError,
+}
+
 fn main() -> Fallible<()> {
     let config_path = env::var("HPOS_CONFIG_PATH")?;
     let registry_url = env::var("ROUTER_REGISTRY_URL")?;
     let config_json = fs::read(config_path)?;
-    let Config::V1 { seed, .. } = serde_json::from_slice(&config_json)?;
-
-    let holochain_secret_key = SecretKey::from_bytes(&seed)?;
-    let holochain_public_key = PublicKey::from(&holochain_secret_key);
-    let holochain_keypair = Keypair {
-        public: holochain_public_key,
-        secret: holochain_secret_key,
-    };
-
-    let zerotier_identity =
-        Identity::try_from(&fs::read_to_string("/var/lib/zerotier-one/identity.secret")?[..])?;
-    let zerotier_address = zerotier_identity.address.clone();
-    let zerotier_keypair: Keypair = zerotier_identity.try_into()?;
-
-    let payload = Payload {
-        instant: SystemTime::now(),
-        holochain_agent_id: holochain_public_key,
-        zerotier_address: zerotier_address,
-    };
-
-    let payload_bytes = serde_json::to_vec(&payload)?;
-
-    let holochain_signature = holochain_keypair.sign(&payload_bytes[..]);
-    let zerotier_signature = zerotier_keypair.sign(&payload_bytes[..]);
-
-    let mut headers = HeaderMap::new();
-
-    headers.insert(
-        HeaderName::from_static("x-holochain-signature"),
-        signature_header(&holochain_signature),
-    );
-
-    headers.insert(
-        HeaderName::from_static("x-zerotier-signature"),
-        signature_header(&zerotier_signature),
-    );
-
-    Client::new()
-        .post(&format!("{}{}", &registry_url, "/v1/update"))
-        .headers(headers)
-        .body(payload_bytes)
-        .send()?;
-
-    Ok(())
+    match serde_json::from_slice(&config_json)? {
+        Config::V2 { seed, .. } => {
+            let holochain_secret_key = SecretKey::from_bytes(&seed)?;
+            let holochain_public_key = PublicKey::from(&holochain_secret_key);
+            let holochain_keypair = Keypair {
+                public: holochain_public_key,
+                secret: holochain_secret_key,
+            };
+            let zerotier_identity = Identity::try_from(
+                &fs::read_to_string("/var/lib/zerotier-one/identity.secret")?[..],
+            )?;
+            let zerotier_address = zerotier_identity.address.clone();
+            let zerotier_keypair: Keypair = zerotier_identity.try_into()?;
+            let payload = Payload {
+                instant: SystemTime::now(),
+                holochain_agent_id: holochain_public_key,
+                zerotier_address: zerotier_address,
+            };
+            let payload_bytes = serde_json::to_vec(&payload)?;
+            let holochain_signature = holochain_keypair.sign(&payload_bytes[..]);
+            let zerotier_signature = zerotier_keypair.sign(&payload_bytes[..]);
+            let mut headers = HeaderMap::new();
+            headers.insert(
+                HeaderName::from_static("x-holochain-signature"),
+                signature_header(&holochain_signature),
+            );
+            headers.insert(
+                HeaderName::from_static("x-zerotier-signature"),
+                signature_header(&zerotier_signature),
+            );
+            Client::new()
+                .post(&format!("{}{}", &registry_url, "/v1/update"))
+                .headers(headers)
+                .body(payload_bytes)
+                .send()?;
+            return Ok(());
+        }
+        _ => return Err(RouterError::ConfigVersionError.into()),
+    }
 }
